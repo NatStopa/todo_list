91bc52a318651e745607af327d8c6f46e4dc65ab
NatStopa <nat.stopa.nat@gmail.com>
2022-01-30 14:41:53 +0100

    Added local storage

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const addIcon = document.querySelector(".plus-button");
 const taskList = document.querySelector(".task-section");
 
+document.addEventListener("DOMContentLoaded", getTodos);
 addIcon.addEventListener("click", addTask);
+taskList.addEventListener("click", completeTask);
 
 function addTask(e) {
   e.preventDefault();
-  const taskInput = document.querySelector(".task-input");
+  let taskInput = document.querySelector(".task-input");
   const userTask = taskInput.value;
   if (userTask.length >= 2) {
+    saveTodos(userTask);
     const task = document.createElement("div");
     task.classList.add("task");
     taskList.appendChild(task);
@@ -15,21 +18,20 @@ function addTask(e) {
     taskText.classList.add("task-text");
     task.appendChild(taskText);
     taskText.innerText = userTask;
+    taskInput.value = "";
     const doneButton = document.createElement("div");
     doneButton.classList.add("done-button");
     task.appendChild(doneButton);
     doneButton.innerHTML = ` <i
-      class="done-icon material-icons"
-      style="
-      font-size: 35px;
-      color: #09151a;
-      "
-      >done</i
-      >`;
+    class="done-icon material-icons"
+    style="
+    font-size: 35px;
+    color: #09151a;
+    "
+    >done</i
+    >`;
   }
 }
-
-taskList.addEventListener("click", completeTask);
 
 function completeTask(e) {
   if (e.target.classList[0] === "done-icon") {
@@ -37,5 +39,61 @@ function completeTask(e) {
     const todoTask = todoButton.parentElement;
     const todoText = todoTask.childNodes[0];
     todoText.classList.toggle("completed");
+    removeTodos(todoTask);
   }
+}
+
+function saveTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function removeTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(function (todo) {
+    let taskInput = document.querySelector(".task-input");
+    const userTask = taskInput.value;
+    const task = document.createElement("div");
+    task.classList.add("task");
+    taskList.appendChild(task);
+    const taskText = document.createElement("p");
+    taskText.classList.add("task-text");
+    task.appendChild(taskText);
+    taskText.innerText = todo;
+    taskInput.value = "";
+    const doneButton = document.createElement("div");
+    doneButton.classList.add("done-button");
+    task.appendChild(doneButton);
+    doneButton.innerHTML = ` <i
+    class="done-icon material-icons"
+    style="
+    font-size: 35px;
+    color: #09151a;
+    "
+    >done</i
+    >`;
+  });
 }
