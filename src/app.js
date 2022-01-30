d8a4b509df42c21a56d7b8825702636be44d8898
NatStopa <nat.stopa.nat@gmail.com>
2022-01-30 13:25:28 +0100

    Added functionality to done button

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const addIcon = document.querySelector(".plus-button");
-const doneIcon = document.getElementsByClassName("done-icon");
+const taskList = document.querySelector(".task-section");
 
 addIcon.addEventListener("click", addTask);
 
@@ -8,7 +8,6 @@ function addTask(e) {
   const taskInput = document.querySelector(".task-input");
   const userTask = taskInput.value;
   if (userTask.length >= 2) {
-    const taskList = document.querySelector(".task-section");
     const task = document.createElement("div");
     task.classList.add("task");
     taskList.appendChild(task);
@@ -27,5 +26,16 @@ function addTask(e) {
       "
       >done</i
       >`;
+  }
+}
+
+taskList.addEventListener("click", completeTask);
+
+function completeTask(e) {
+  if (e.target.classList[0] === "done-icon") {
+    const todoButton = e.target.parentElement;
+    const todoTask = todoButton.parentElement;
+    const todoText = todoTask.childNodes[0];
+    todoText.classList.toggle("completed");
   }
 }
